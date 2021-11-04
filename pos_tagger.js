c1117020a33d5ed1304143d1f6972f9f7eaed03a
Aviral Srivastava <aviralji4@gmail.com>
2021-11-04 19:05:14 +0530

    Update pos_tagger.js

--- pos_tagger.js
+++ pos_tagger.js
@@ -1,10 +1,28 @@
+// basic POS tagging
 // const request = require('request');
-// const cheerio = require('cheerio');
+// wordpos = new WordPOS();
+// var content = 'The angry bear chased the frightened little squirrel.';
+// wordpos.getPOS(content, function(result){
+//     console.log(result);
+// });
+
+const request = require('request');
+const cheerio = require('cheerio');
 const WordPOS = require('wordpos'),
 
 wordpos = new WordPOS();
-var content = 'The angry bear chased the frightened little squirrel.';
+var wordMap = {};
 
-wordpos.getPOS(content, function(result){
-    console.log(result);
-});
+const scrap = (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const content = $('p');
+        const wordcontent = content.text();
+        wordpos.getPOS(wordcontent, function(result){
+            console.log(result);
+        });
+    }
+};
+
+const search = "Taj mahal".split(" ").join("_");
+request(`https://en.wikipedia.org/wiki/${search}`, scrap);
